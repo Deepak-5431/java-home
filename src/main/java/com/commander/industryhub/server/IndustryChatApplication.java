237af94f0dbde78120f7a82494cd183846a4bf5a
Deepak-5431 <deepakchand540@gmail.com>
2025-10-26 17:11:15 +0530

    first

---
+++ src/main/java/com/commander/industryhub/server/IndustryChatApplication.java
@@ -0,0 +1,13 @@
+package com.commander.industryhub.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustryChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndustryChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/commander/industryhub/server/IndustryChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commander.industryhub.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustryChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
