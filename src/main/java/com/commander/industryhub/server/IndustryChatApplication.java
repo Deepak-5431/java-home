afe29d8f82b14646b0c3a9477bb4dae5ed856773
Deepak-5431 <deepakchand540@gmail.com>
2025-11-11 16:30:32 +0530

    User model is completed

--- src/main/java/com/commander/industryhub/server/IndustryChatApplication.java
+++ src/main/java/com/commander/industryhub/server/IndustryChatApplication.java
@@ -2,8 +2,10 @@ package com.commander.industryhub.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class IndustryChatApplication {
 
     public static void main(String[] args) {
